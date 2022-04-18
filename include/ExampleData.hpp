cd7800e96019a9422a50b73a9b8ad4200e1fd16a
TSprech <trey.sprecher@gmail.com>
2022-04-17 20:47:33 -0400

    Converted Stress Test File to Hex
    Instead of having the utf stress test file use the invalid characters as they are encoded, they are now in u8 strings using hex representation so the invalid unicode doesn't have to be read in as a file.

--- include/ExampleData.hpp
+++ include/ExampleData.hpp
@@ -20,23 +20,11 @@ enum class Languages : uint16_t {
   Japanese
 };
 
-const char8_t* const translated_strings[] = {u8"Hello", // English
-                                             u8"¿Qué tal?", // Spanish
-                                             u8"Halløj", // Danish
-                                             u8"こんにちは", // Japanese
-                                             // The following are taken from Markus Kuhn's UTF-8 stress test file
-                                             u8"\x80", // First continuation byte
-                                             u8"\xBF", // Last continuation byte
-                                             u8"\xC0 ", // 2 Byte sequence first byte followed by space
-                                             u8"\xE0 ", // 3 Byte sequence first byte followed by space
-                                             u8"\xC0", // 8: 2 Byte sequence with last byte missing
-                                             u8"\xF0\x80\x80", // 4 Byte sequence with last byte missing
-                                             u8"\xFE", // Impossible byte
-                                             u8"\xFF", // Impossible byte
-                                             u8"\xC0\xAF", // Overlong sequence
-                                             u8"\xF0\x80\x80\xAF", // Overlong sequence
-                                             u8"\xED\xA0\x80", // Invalid characters
-                                             u8""
+const char8_t* const translated_strings[] = {
+    u8"Hello",       // English
+    u8"¿Qué tal?",   // Spanish
+    u8"Halløj",      // Danish
+    u8"こんにちは",  // Japanese
 };
 
-#endif // EXAMPLEDATA_HPP
+#endif  // EXAMPLEDATA_HPP
